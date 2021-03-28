33e5a628257bd94a0845144f67daa32c27123087
jamyxe <62660338+jamyxe@users.noreply.github.com>
2021-03-28 14:39:31 +0800

    Initial commit

---
+++ 01/a.py
@@ -0,0 +1,10 @@
+'''
+Version      : 1.0
+Autor        : jamxe
+Created on   : 2021-02-04 22:31:22
+Last Modified: 2021-02-04 22:32:38
+FilePath     : /PyWorks/01/a.py
+Description  : python beginner
+'''
+import numpy as np
+np.__config__.show()
