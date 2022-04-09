b3d5491817fd64bfe51fdc070ca21cc27b891fb0
jamxe <62660338+jamxe@users.noreply.github.com>
2022-04-09 21:18:43 +0800

    changes2
    changes2

---
+++ python_hello/main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    dict1 = {}
+    dict2 = {3: 5}
+    dioct4 = dict(([2, 4], [2, 3]))
+    s = 'a\nb\tc'
+    print(len(s))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
