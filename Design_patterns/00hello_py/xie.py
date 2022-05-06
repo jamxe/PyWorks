43f59db2dd979fc4767f576dab7240d7eb6f75f8
jamxe <62660338+jamxe@users.noreply.github.com>
2022-05-06 22:32:04 +0800

    observer_test

---
+++ Design_patterns/00hello_py/xie.py
@@ -0,0 +1,10 @@
+'''
+Version      : 1.0
+Autor        : jamyxe
+Created on   : 2022-04-26 21:31:47
+Last Modified: 2022-04-26 21:36:36
+FilePath     : /PyWorks/Design_patterns/00hello_py/xie.py
+Description  : 
+'''
+hello python
+ceshi 
